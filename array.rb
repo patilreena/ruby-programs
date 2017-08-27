c9a1467ec5475a1cfdbf9d14f6dea0eeea6002df
Reena Patil <reena.patil@outlook.com>
2017-08-27 23:58:26 +1000

    Finding array count

---
+++ array.rb
@@ -0,0 +1,20 @@
+  shoppingList = [
+  "chips",
+  "cake",
+  "water",
+  "fruits",
+  alcohol =[
+    "Tooheys old",
+    "Rum",
+    "Gin",
+    "Tequila",
+    "Whiskey"
+  ]
+]
+
+item_name = shoppingList[3]
+listCount = shoppingList.length + alcohol.length
+
+puts "The item name is #{item_name}"
+
+puts "My shopping list has #{listCount} items"
