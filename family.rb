d5c29d72515f71390aca67d2e9207dfcc5602327
Reena Patil <reena.patil@outlook.com>
2017-08-28 00:37:38 +1000

    a ruby program that displays a  number of family members

---
+++ family.rb
@@ -0,0 +1,24 @@
+familyMembers = [
+  "dad",
+  "mom",
+  "sister",
+  "brother",
+  "husband",
+  friends = [
+    "Daniel",
+    "Aiden",
+    "David",
+    "Oliver",
+    "Luke"
+  ]
+]
+
+sweetHeart = familyMembers[1]
+count = familyMembers.length
+bestFriend = friends[4]
+
+puts "My sweetHeart is my #{sweetHeart}"
+
+puts "number of peoples in our family is #{count}"
+
+puts "My bestFriend is #{bestFriend}"
